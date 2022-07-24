5449e2e8caea834a2c8abf84bc44b44d233afbe4
Carter Dillman <carterdillman@hotmail.com>
2022-07-23 20:43:34 -0600

    Added Staff members, lightbox, page titles, and .inset

--- src/UI/Pages/Staff/Staff.jsx
+++ src/UI/Pages/Staff/Staff.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
+/* Componenets -----------------------------*/
+import StaffList from './StaffList.jsx';
+
 const Staff = () => {
     return (
-        <StaffStyled className='Staff'>
-            Staff
+        <StaffStyled className='Staff inset'>
+            <h1>Staff</h1>
+            <StaffList />
         </StaffStyled>
     );
 }
