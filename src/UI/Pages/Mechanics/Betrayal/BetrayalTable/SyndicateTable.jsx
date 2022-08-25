ccf97ee04437d27a9621f99d24c8ffe12b824217
Carter Dillman <carterdillman@hotmail.com>
2022-08-25 16:44:15 -0600

    Adjusted table data

--- src/UI/Pages/Mechanics/Betrayal/BetrayalTable/SyndicateTable.jsx
+++ src/UI/Pages/Mechanics/Betrayal/BetrayalTable/SyndicateTable.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 /* Scripts ---------------------------*/
-import { memberData } from 'UI/common/data/syndicateData';
+import { memberData, imageData } from 'UI/common/data/syndicateData';
 
 /* Components ---------------------------*/
 import BetrayalTable from './BetrayalTable.jsx';
@@ -12,14 +12,19 @@ const SyndicateTable = () => {
     const columns = React.useMemo(
         () => [
             {
-                Header: '',
-                accessor: 'name',
-            },
+                Header: ' ',
+                Cell: tableProps => (
+                    <img
+                        src={tableProps.row.original.aisling}
+                    />
+                )
+            }
         ]
     )
+
     return (
         <SyndicateTableStyled className='SyndicateTable'>
-            <BetrayalTable columns={columns} data={memberData}/>
+            <BetrayalTable columns={columns} data={memberData} />
         </SyndicateTableStyled>
     );
 }
--- src/UI/common/data/syndicateData.js
+++ src/UI/common/data/syndicateData.js
@@ -1,10 +1,10 @@
 export const memberData = [
     {
-        name: 'Aisling',
-        image: '/assets/img/incursion/SacrificialChamber.png',
+        name: '',
+        aisling: '/assets/img/incursion/SacrificialChamber.png',
     },
     {
-        name: 'Aisling',
-        image: '/assets/img/incursion/SacrificialChamber.png',
+        name: 'Transportation',
+        aisling: '/assets/img/incursion/SacrificialChamber.png',
     },
 ]
